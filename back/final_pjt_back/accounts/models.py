0330a4aa48f7d8eed8b2ed4220796b05aff12160
양건우 <mywe2365@gmail.com>
2023-11-21 12:13:08 +0900

    yang

--- back/final_pjt_back/accounts/models.py
+++ back/final_pjt_back/accounts/models.py
@@ -7,15 +7,15 @@ class CustomUserManager(UserManager):
     pass
 
 class User(AbstractUser):
-    username = models.CharField(max_length=30) 
-    nickname = models.CharField(max_length=255, unique=True)
+    username = models.CharField(max_length=254, unique=True) 
+    nickname = models.CharField(max_length=254)
     email = models.EmailField(max_length=254, blank=True, null=True)
     age = models.IntegerField()
     salary = models.IntegerField(blank=True, null=True)
     money = models.FloatField(blank=True, null=True)
     gender = models.BooleanField()
     address = models.TextField(blank=True, null=True)
-    USERNAME_FIELD = 'nickname'
+    USERNAME_FIELD = 'username'
     # superuser fields
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
