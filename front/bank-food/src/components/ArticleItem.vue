c3ebc71fb21f2d8c5e028a9f67599d68797f6090
suehwanBoo <rndjdieo119@naver.com>
2023-11-22 22:05:39 +0900

    1122

---
+++ front/bank-food/src/components/ArticleItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <tr>
+        <td>{{ article.author_name }}</td>
+        <td>{{ article.title }}</td>
+        <td>{{ article.created_at }}</td>
+        <td>{{ article.updated_at }}</td>
+
+
+    </tr>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+defineProps(['article'])
+
+</script>
+
+<style scoped>
+
+</style>
